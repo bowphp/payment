491e69002f12acb222e053843ebd7c49d5652482
Franck DAKIA <dakiafranck@gmail.com>
2020-01-15 10:05:16 +0000

    feat: add editorconfig

--- src/OrangeMoney/OrangeMoney.php
+++ src/OrangeMoney/OrangeMoney.php
@@ -12,70 +12,70 @@ class OrangeMoney
     * @var HttpClient
     */
     private $http;
-
+    
     /**
-     * The pay route
-     * 
-     * @var string
-     */
+    * The pay route
+    * 
+    * @var string
+    */
     private $pay_url = 'https://api.orange.com/orange-money-webpay/dev/v1/webpayment';
-
+    
     /**
-     * The generate orange money token
-     * 
-     * @var OrangeMoneyToken
-     */
+    * The generate orange money token
+    * 
+    * @var OrangeMoneyToken
+    */
     private $token;
-
+    
     /**
-     * The return url
-     * 
-     * @var string
-     */
+    * The return url
+    * 
+    * @var string
+    */
     private $return_url;
-
+    
     /**
-     * The cancel payment url redirection
-     * 
-     * @var string
-     */
+    * The cancel payment url redirection
+    * 
+    * @var string
+    */
     private $cancel_url;
-
+    
     /**
-     * The notify url for successful payment
-     * 
-     * @var string
-     */
+    * The notify url for successful payment
+    * 
+    * @var string
+    */
     private $notif_url;
-
+    
     /**
-     * The merchand id
-     * 
-     * @var string
-     */
+    * The merchand id
+    * 
+    * @var string
+    */
     private $merchant_id;
-
+    
     /**
-	 * OrangeMoney contructor
-	 * 
-	 * @return mixed
-	 */
-	public function __construct(OrangeMoneyToken $token, $merchant_id)
-	{
+    * OrangeMoney contructor
+    * 
+    * @return mixed
+    */
+    public function __construct(OrangeMoneyToken $token, $merchant_id)
+    {
         $this->token = $token;
-
+        
         $this->http = new HttpClient;
-
+        
         $this->merchant_id = $merchant_id;
-
+        
         $this->currency = 'OUV';
     }
-
+    
     /**
-     * Make payment
-     * 
-     * @return 
-     */
+    * Make payment
+    * 
+    * @return 
+    */
     public function pay($amount, $reference, $order_id)
     {
         $response = $this->http->post($this->pay_url, [
@@ -92,47 +92,48 @@ class OrangeMoney
             ],
             "headers" => [
                 "Authorization" => $this->token
-            ]
-        ]);
-    }
-
-    /**
-     * Set the return url when the payment have successful
-     * 
-     * @param string $url
-     */
-    public function setReturnUrl($url)
-    {
-        $this->return_url = $url;
-    }
-
-    /**
-     * Set the notify payment url when it's successful
-     * 
-     * @param string $url
-     */
-    public function setNotifyUrl($url)
-    {
-        $this->notif_url = $url;
-    }
-
-    /**
-     * Set the cancel payment url redirection
-     * 
-     * @param string $url
-     */
-    public function setCancelUrl($url)
-    {
-        $this->cancel_url = $url;
-    }
-
-    /**
-     * Set the payment currency
-     * 
-     * @param string $currency
-     */
-    public function setCurrency($currency)
-    {
-        $this->currency = $currency;
-    }
-}
+                ]
+                ]);
+            }
+            
+            /**
+            * Set the return url when the payment have successful
+            * 
+            * @param string $url
+            */
+            public function setReturnUrl($url)
+            {
+                $this->return_url = $url;
+            }
+            
+            /**
+            * Set the notify payment url when it's successful
+            * 
+            * @param string $url
+            */
+            public function setNotifyUrl($url)
+            {
+                $this->notif_url = $url;
+            }
+            
+            /**
+            * Set the cancel payment url redirection
+            * 
+            * @param string $url
+            */
+            public function setCancelUrl($url)
+            {
+                $this->cancel_url = $url;
+            }
+            
+            /**
+            * Set the payment currency
+            * 
+            * @param string $currency
+            */
+            public function setCurrency($currency)
+            {
+                $this->currency = $currency;
+            }
+        }
+        
--- src/OrangeMoney/OrangeMoneyPaymentStatus.php
+++ src/OrangeMoney/OrangeMoneyPaymentStatus.php
@@ -5,43 +5,44 @@ namespace Bow\Payment\OrangeMoney;
 class OrangeMoneyPaymentStatus
 {
     /**
-     * OrangeMoneyPaymentStatus constructor
-     * 
-     * @param string $payment_url
-     * @param string $pay_token
-     * @param string $notif_token
-     */
+    * OrangeMoneyPaymentStatus constructor
+    * 
+    * @param string $payment_url
+    * @param string $pay_token
+    * @param string $notif_token
+    */
     public function __construct($payment_url, $pay_token, $notif_token)
     {
         $this->pay_token = $pay_token;
-
+        
         $this->payment_url = $payment_url;
-
+        
         $this->notif_token = $notif_token;
     }
-
+    
     /**
-     * Redirect client to make payment
-     * 
-     * @return mixed
-     */
+    * Redirect client to make payment
+    * 
+    * @return mixed
+    */
     public function redirect()
     {
         header('Location: ' . $this->payment_url);
         die();
     }
-
+    
     /**
-     * __toString
-     * 
-     * @return string
-     */
+    * __toString
+    * 
+    * @return string
+    */
     public function __toString()
     {
         return json_encode([
             $this->pay_token,
             $this->payment_url,
             $this->notif_token
-        ]);
+            ]);
+        }
     }
-}
+    
