87700b684b5ee41ae1cff64a30fcfd40e4a516d0
Franck DAKIA <dakiafranck@gmail.com>
2020-01-20 23:39:18 +0000

    feat: add unity test

--- tests/OrangeMoneyTest.php
+++ tests/OrangeMoneyTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Bow\Payment\OrangeMoney\OrangeMoney;
+use Bow\Payment\OrangeMoney\OrangeMoneyPaymentStatus;
 use Bow\Payment\OrangeMoney\OrangeMoneyToken;
 use Bow\Payment\OrangeMoney\OrangeMoneyTokenGenerator;
 
@@ -13,5 +15,21 @@ class OrangeMoneyTest extends \PHPUnit\Framework\TestCase
             ->disableOriginalConstructor()->getMock());
 
         $this->assertInstanceOf(OrangeMoneyToken::class, $stub->getToken());
+    }
+
+    public function testMakePayment()
+    {
+        $token = $this->getMockBuilder(OrangeMoneyToken::class)
+            ->disableOriginalConstructor()->getMock();
+        
+        $orange = $this->getMockBuilder(OrangeMoney::class)
+            ->setConstructorArgs([$token, 123456])
+            ->setMethods(['pay'])->getMock();
+
+        $payment_status = $this->createMock(OrangeMoneyPaymentStatus::class);
+        $orange->method('pay')
+            ->willReturn($payment_status);
+
+        $this->assertInstanceOf(OrangeMoneyPaymentStatus::class, $orange->pay(500, 'reference', 1));
     }
 }
