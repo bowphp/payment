7b1dd4b66ad0918e3eb9061ff4761576e68e23d4
Franck DAKIA <dakiafranck@gmail.com>
2020-01-21 18:38:46 +0000

    feat: init packaging

---
+++ src/PaymentConfirguration.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bow\Payment;
+
+use Bow\Configuration\Configuration;
+
+class PaymmentConfirguration extends Configuration
+{
+    /**
+     * Create payment configuration
+     * 
+     * @param \Bow\Configuration\Loader $config
+     */
+    public function create(\Bow\Configuration\Loader $config)
+    {
+        //
+    }
+
+    /**
+     * Launch configuration
+     * 
+     * @return mixed
+     */
+    public function run()
+    {
+        //
+    }
+}
---
+++ src/PaymentNotificationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bow\Payment;
+
+use App\Controller\Controller;
+use Bow\Http\Request;
+
+class PaymentNotificationController extends Controller
+{
+    /**
+     * Process payment notification here
+     * 
+     * @param Request $request
+     * @return mixed
+     */
+    public function handle(Request $request)
+    {
+        //
+    }
+}
